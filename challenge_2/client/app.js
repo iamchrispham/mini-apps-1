4b80063f088ec86014679f4143f6bb9c13e94520
iamchrispham <chrismpham@gmail.com>
2019-06-25 20:02:51 -0700

    commit#2
    now on the step to convert JSON data as a CSV report back to client side.

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,23 +1,22 @@
+
 handleOnSubmit = () => {
-
+  var formData = document.getElementById("form-text").value.split(', ');
+  var JSONObj = { // firstName,lastName,county,city,role,sales
+    firstName: formData[0],
+    lastName: formData[1],
+    county: formData[2],
+    city: formData[3],
+    role: formData[4],
+    sales: formData[5],
+    children: []
+  };
+  var JSONStr = JSON.stringify(JSONObj);
+  var xml = new XMLHttpRequest();
+  xml.responseType = 'json';
+  xml.open('POST', '/', true);
+  xml.setRequestHeader('Content-type', 'Application/json')
+  xml.onload = function () {
+    console.log('RESPONSE FROM SERVER:', this.response);
+  }
+  xml.send(JSONStr);
 }
-
-// sending a request without AJAX/JQuery
-// handleOnSubmit = () => {
-//   var formData = document.getElementById("form-text").value.split(', ');
-//   var JSONObj = { // firstName,lastName,county,city,role,sales
-//     firstName: formData[0],
-//     lastName: formData[1],
-//     county: formData[2],
-//     city: formData[3],
-//     role: formData[4],
-//     sales: formData[5],
-//     children: []
-//   };
-  
-//   var xml = new XMLHttpRequest();
-//   xml.open('POST', '/', true);
-//   xml.setRequestHeader('Content-type', 'Application/json')
-//   xml.send(JSON.stringify(JSONObj));
-//   console.log('Hello! ', JSON.stringify(JSONObj));
-// }
--- challenge_2/server/server.js
+++ challenge_2/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser'); // middleware
 const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
@@ -13,12 +14,27 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error("ERROR: ", err);
+      res.end();
+    } else {
+      console.log('DATA:', data);
+      // need to create a recursive function to go through tree
+      // go through data of tree
+      // break down each object of the tree
+      // send as CSV data back to client
+      res.status(201).send(data);
+    }
+  })
   var { firstName, lastName, county, city, role, sales } = req.body
   console.log('test:', req.body);
-  res.status(201).send(req.body)
+  // res.status(201).send(req.body)
 })
 
 app.listen(port, () => { console.log(`Listening to port ${port}...`)})
 
+const filePath = path.join(__dirname, "../samples/sales_report.json")
+console.log('FILEPATH to SALES_REPORT:', filePath);
 
 
